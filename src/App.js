5f86de5e25a38d27775e022384651608ecb8a7af
reinhardtwijaya <Reinhardt@appsure.com.au>
2025-06-17 15:22:54 +1000

    Add Logo to nav bar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
+// 1. Import your logo from the src folder
+import myLogo from './my_logo_r.png'; 
 
 function App() {
   return (
     <div className="App">
       {/* Navigation Bar */}
       <nav className="navbar">
+        {/* ADDED LOGO HERE */}
+        <a href="#home" className="navbar-logo">
+          {/* 2. Use the imported logo variable here */}
+          <img src={myLogo} alt="logo" />
+        </a>
         <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li><a href="#tech">Tech</a></li>
+          <li><a href="#cooking">Cooking</a></li>
+          <li><a href="#thoughts">Thoughts</a></li>
+          <li><a href="#Contact">Contact</a></li>
         </ul>
       </nav>
 
